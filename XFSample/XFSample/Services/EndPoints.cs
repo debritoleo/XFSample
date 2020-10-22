21f5cf53e092fd02d0926adb765c55c3d8c7c36f
Leonardo de brito <leo_guty11@hotmail.com>
2020-10-21 23:55:42 -0300

    included endpoints class with baseurl

---
+++ XFSample/XFSample/Services/EndPoints.cs
@@ -0,0 +1,7 @@
+﻿namespace XFSample.Services
+{
+    public static class EndPoints
+    {
+        public static string BaseUrl => "https://run.mocky.io/v3/83599a37-9b03-47d1-970d-555f8835355c";
+    }
+}
